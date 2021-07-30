b379e53763d1aaa76690902bb718d5576af35c21
Evgeniy Kurilenko <kurilenko.e.m@gmail.com>
2021-07-31 00:08:59 +0300

    initial commit

---
+++ src/main/java/com/burlywoodpalette/bwpalette/BWPaletteApplication.java
@@ -0,0 +1,13 @@
+package com.burlywoodpalette.bwpalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BWPaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BWPaletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burlywoodpalette/bwpalette/BWPaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burlywoodpalette.bwpalette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BWPaletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
